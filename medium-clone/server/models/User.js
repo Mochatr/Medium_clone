7facf5e0378e2bcaa0fe92eab5ebed94305b4cec
Mochatr <Mohamedchatr8@gmail.com>
2024-04-16 16:44:42 +0100

    server side setup

---
+++ medium-clone/server/models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+    {
+        name: String,
+        email: String,
+        provider: String,
+        provider_id: String,
+        token: String,
+        provider_pic: String,
+        followers: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        following: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+)
+userSchema.methods.follow = function(user_id) {
+    if (this.following.indexOf(user_id) === -1) {
+        this.following.push(user_id)
+    }
+    return this.save()
+}
+userSchema.methods.addFollower = function(fs) {
+    this.followers.push(fs)
+}
+export default model('User', userSchema);
